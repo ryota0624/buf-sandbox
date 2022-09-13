94d1d2659b5355e3902142840d912e7f0cfd7307
ryota0624 <tennis.suta6@gmail.com>
2022-09-13 14:36:04 +0900

    dartのproject scaffolding

---
+++ bin/buf_sandbox.dart
@@ -0,0 +1,5 @@
+import 'package:buf_sandbox/buf_sandbox.dart' as buf_sandbox;
+
+void main(List<String> arguments) {
+  print('Hello world: ${buf_sandbox.calculate()}!');
+}
---
+++ test/buf_sandbox_test.dart
@@ -0,0 +1,8 @@
+import 'package:buf_sandbox/buf_sandbox.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
